7d1fee07fc3850dc0046b5aad564aa8e15b88bc3
AbimaelCorujeira <corujeira.abimael@gmail.com>
2023-02-01 16:49:54 -0300

    class Coard and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,11 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
 
-        Position p = new Position(3, 5);
-        System.out.println(p);
-
+        Board board = new Board(8, 8);
     }
 }
---
+++ src/boardgame/Board.java
@@ -0,0 +1,22 @@
+package boardgame;
+
+public class Board {
+
+    private int rows;
+    private int columns;
+    private Piece[][] pieces;
+
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+}
