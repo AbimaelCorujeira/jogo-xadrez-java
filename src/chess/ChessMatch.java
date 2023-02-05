eedaac2696610d0517c8628679f0e0727497d31a
AbimaelCorujeira <corujeira.abimael@gmail.com>
2023-02-05 16:27:20 -0300

    Possíveis movimentos de uma peça

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -43,8 +43,13 @@ public class ChessMatch {
 
     private void validateSourcePosition(Position position) {
         if(!board.thereIsAPiece(position)) {
-            throw new ChessException("Não existe peça na posição de origem");
+            throw new ChessException("Nao existe peca na posicao de origem");
+        } if(!board.piece(position).isThereAnyPossibleMove()) {
+            throw new ChessException("Nao existe movimentos possiveis para a peca escolhida");
+
+
         }
+
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
