b20684d627b58059e781a0a391a7a9a6fd7098be
AbimaelCorujeira <corujeira.abimael@gmail.com>
2023-02-07 23:00:12 -0300

    correção de bugs

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -78,7 +78,7 @@ public class ChessMatch {
 
         if (testCheck(currentPlayer)) {
             undoMove(source, target, capturedPiece);
-            throw new ChessException("You can't put yourself in check");
+            throw new ChessException("Voce nao pode se colocar em cheque");
         }
 
         ChessPiece movedPiece = (ChessPiece)board.piece(target);
@@ -198,19 +198,19 @@ public class ChessMatch {
 
     private void validateSourcePosition(Position position) {
         if (!board.thereIsAPiece(position)) {
-            throw new ChessException("There is no piece on source position");
+            throw new ChessException("Não há peça na posição de origem");
         }
         if (currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
-            throw new ChessException("The chosen piece is not yours");
+            throw new ChessException("A peca escolhida nao e sua");
         }
         if (!board.piece(position).isThereAnyPossibleMove()) {
-            throw new ChessException("There is no possible moves for the chosen piece");
+            throw new ChessException("Nao ha movimentos possiveis para a peaa escolhida");
         }
     }
 
     private void validateTargetPosition(Position source, Position target) {
         if (!board.piece(source).possibleMove(target)) {
-            throw new ChessException("The chosen piece can't move to target position");
+            throw new ChessException("A peca escolhida nao pode se mover para a posicao alvo");
         }
     }
 
@@ -230,7 +230,7 @@ public class ChessMatch {
                 return (ChessPiece)p;
             }
         }
-        throw new IllegalStateException("There is no " + color + " king on the board");
+        throw new IllegalStateException("Não há rei " + color + "no tabuleiro");
     }
 
     private boolean testCheck(Color color) {
